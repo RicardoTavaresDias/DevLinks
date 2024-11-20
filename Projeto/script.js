e190e87cfe462b0f47a87fdd47e5a710109f399a
“RicardoTavaresDias” <ricardo.ti.sistemas@gmail.com>
2024-11-20 18:13:27 +0000

    first commit

---
+++ Projeto/script.js
@@ -0,0 +1,35 @@
+
+
+function toggleMode() {
+  const html = document.documentElement
+
+  //realizando uma função para executar no momento do click
+  if (html.classList.contains("light")) {
+    html.classList.remove("light")
+  } else {
+    html.classList.add("light")
+  }
+
+  //Outra forma que faz mesma coisa
+  //html.classList.toggle('light')
+
+  //pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  //substituir a imagem
+  if (html.classList.contains("light")) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar.png")
+    
+    //realizando mesmo procedimento na descrição do ALT da imagem
+    img.setAttribute("alt", "Foto de perfil com oculos escuro")
+
+  } else {
+    //se tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar_light.png")
+
+    //realizando mesmo procedimento na descrição do ALT da imagem
+    img.setAttribute("alt", "Foto de perfil com oculos de grau")
+  }
+
+}
