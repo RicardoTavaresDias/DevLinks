a697d199ed5f56eddabf9e470b5ad8f86044281d
“RicardoTavaresDias” <ricardo.ti.sistemas@gmail.com>
2024-11-20 22:15:14 +0000

    Realizado remoção das bordas de argem, substituição das foto do perfil, tela inicial alterada para noite.

--- Projeto/script.js
+++ Projeto/script.js
@@ -19,17 +19,17 @@ function toggleMode() {
   //substituir a imagem
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/avatar_dia.png")
     
     //realizando mesmo procedimento na descrição do ALT da imagem
-    img.setAttribute("alt", "Foto de perfil com oculos escuro")
+    img.setAttribute("alt", "Foto de perfil com oculos escuro, esta dia")
 
   } else {
     //se tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "./assets/avatar_light.png")
+    img.setAttribute("src", "./assets/avatar_noite.png")
 
     //realizando mesmo procedimento na descrição do ALT da imagem
-    img.setAttribute("alt", "Foto de perfil com oculos de grau")
+    img.setAttribute("alt", "Foto de perfil com oculos de grau, esta noite")
   }
 
 }
